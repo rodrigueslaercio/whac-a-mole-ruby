d7cd45e655bea22a0f070912553221682eb1ca45
savate <gvzmxdj@gmail.com>
2023-02-06 23:21:36 -0300

    Change difficulty at 60s

--- whack_a_mole.rb
+++ whack_a_mole.rb
@@ -5,6 +5,7 @@ class WhackAMole < Gosu::Window
     super(800, 600)
     self.caption = 'Whack a mole!'
     @mole = Gosu::Image.new('media/mole.png')
+    @second_mole = Gosu::Image.new('media/mole.png')
     @hammer = Gosu::Image.new('media/hammer.png')
     @x = 200
     @y = 200
@@ -18,6 +19,8 @@ class WhackAMole < Gosu::Window
     @score = 0
     @playing = true
     @start_time = 0
+    @hits = 0
+    @wrongs = 0
   end
 
   def draw
@@ -25,7 +28,7 @@ class WhackAMole < Gosu::Window
       @mole.draw(@x - @width / 2, @y - @height / 2, 1)
     end
     @hammer.draw(mouse_x - 120, mouse_y - 45, 1)
-    if @hit == 0
+    if @hit.zero?
       c  = Gosu::Color::NONE
     elsif @hit == 1
       c = Gosu::Color::GREEN
@@ -34,8 +37,10 @@ class WhackAMole < Gosu::Window
     end
     draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
     @hit = 0
-    @font.draw_text(@score.to_s, 700, 20, 2)
-    @font.draw_text(@time_left.to_s, 20,20,2)
+    @font.draw_text("Score: #{@score}", 650, 20, 2)
+    @font.draw_text("Time left: #{@time_left}", 20,20,2)
+    @font.draw_text("Hits: #{@hits} ", 20,70,2)
+    @font.draw_text("Errors: #{@wrongs}", 20, 100, 2)
     unless @playing
       @font.draw_text('Game Over', 300, 300, 3)
       @font.draw_text('Press Space Bar to Play Again', 175, 350, 3)
@@ -51,12 +56,23 @@ class WhackAMole < Gosu::Window
     if @playing
       if id == Gosu::MsLeft
         if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+          if @time_left <= 60
+            @score += 2
+          else
+            @score += 5
+          end
+          @hits += 1
           @hit = 1
-          @score += 5
           @visible = -1
         else
+          @time_left -= 10
+          if @time_left <= 60
+            @score -= 15
+          else
+            @score -= 1
+          end
           @hit = -1
-          @score -= 1
+          @wrongs += 1
           @visible = -1
         end
       end
@@ -66,6 +82,8 @@ class WhackAMole < Gosu::Window
         @visible = -10
         @start_time = Gosu.milliseconds
         @score = 0
+        @hits = 0
+        @wrongs = 0
       end
     end
   end
@@ -77,9 +95,14 @@ class WhackAMole < Gosu::Window
       @velocity_x *= -1 if @x + @width / 2 > 800 || (@x - @width / 2).negative?
       @velocity_y *= -1 if @y + @height / 2 > 600 || (@y - @height / 2).negative?
       @visible -= 1
-      @visible = 30 if @visible < - 10 && rand < 0.01
       @time_left = (100 - ((Gosu.milliseconds - @start_time) / 1000))
-      @playing = false if @time_left == 0
+      @playing = false if @time_left.zero?
+
+      if @time_left <= 60
+        @visible = 30 if @visible < - 10 && rand < 0.05
+      else
+        @visible = 30 if @visible < - 10 && rand < 0.01
+      end
     end
   end
 end
